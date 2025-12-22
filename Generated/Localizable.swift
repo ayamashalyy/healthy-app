693b961a190f64eb50de5630cc8e61586baea23f
aya <ayamashaly363@gmail.com>
2025-12-22 20:46:51 +0200

    chore: configure SwiftGen for strings, images, and colors

---
+++ Generated/Localizable.swift
@@ -0,0 +1,42 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+internal enum L10n {
+  /// Login
+  internal static let loginTitle = L10n.tr("Localizable", "login_title", fallback: "Login")
+  /// Sign Up
+  internal static let signupButton = L10n.tr("Localizable", "signup_button", fallback: "Sign Up")
+  /// Welcome to Healthy App!
+  internal static let welcomeMessage = L10n.tr("Localizable", "welcome_message", fallback: "Welcome to Healthy App!")
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
