181f6c1cdcbf25294314b6335b8fbb879d07cbc4
aya <ayamashaly363@gmail.com>
2025-12-23 13:28:45 +0200

    Keep empty folder structure

--- Healthy/Classes/SceneDelegate.swift
+++ Healthy/Classes/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let viewController = UIViewController()
-        viewController.view.backgroundColor = .blue
+        viewController.view.backgroundColor = .white
         
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = viewController
